3c57497e62b929d4f6db8ecbcf2ed66398131add
chizu praise <chizupraise31@gmail.com>
2024-06-20 08:30:17 -0700

    Initial commit with my code

---
+++ src/components/Education/Education.jsx
@@ -0,0 +1,131 @@
+import React, { Component } from 'react';
+import classes from './Education.module.css';
+// import ScrollAnimation from 'react-animate-on-scroll';
+import 'animate.css/animate.min.css';
+import { MdSchool } from 'react-icons/md';
+import { MdWork } from 'react-icons/md';
+import { FaSchool } from 'react-icons/fa';
+
+class Education extends Component {
+  render() {
+    return (
+      <div className={classes.box} id='education'>
+        {/* <ScrollAnimation
+          offset={0}
+          animateIn='fadeInLeft'
+          duration={2}
+          animateOnce={true}
+          initiallyVisible={true}
+        > */}
+        <span className={classes.head}>MY JOURNEY</span>
+        <section className={classes.container}>
+          <div>
+            <div className={classes.row}>
+              <div className={classes.row_md_12}>
+                <div className={classes.timeline_centered}>
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                  <article className={classes.timeline_entry}>
+                    <div className={`${classes.timeline_icon} ${classes.timeline_icon_5}`}>
+                      <MdWork />
+                    </div>
+                    <div className={classes.label}>
+                      <h2>
+                        Experience <span>june(2023)-Present</span>
+                      </h2>
+                      <p>
+                        I am currently working as <b>Software Developer </b> at{' '}
+                        <a target='_blank' href='https://wiibi.org/'>
+                          <b>wiibi.org</b>
+                        </a>{' '}
+                        <i>
+                          (wiibi is a  technology startup in the real estate industry that is
+                          building tools and products to simplify house hunting and managing of property remotely 
+                       )
+                        </i>{' '}
+                        and boosting my professional skills.
+                      </p>
+                    </div>
+                  </article>
+                  {/* </ScrollAnimation> */}
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                  <article className={classes.timeline_entry}>
+                    <div className={classes.timeline_icon}>
+                      <MdSchool />
+                    </div>
+                    <div className={classes.label}>
+                      <h2>
+                        Undergraduate at National Open University <span></span>
+                      </h2>
+                      <p>
+                       Currently studying computer science at (NOUN) National Open University 
+                      </p>
+                    </div>
+                  </article>
+                  {/* </ScrollAnimation> */}
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                  <article className={classes.timeline_entry}>
+                    <div className={`${classes.timeline_icon} ${classes.timeline_icon_2}`}>
+                      <FaSchool />
+                    </div>
+                    <div className={classes.label}>
+                      <h2>
+                        Secondary  Education <span>2020-2022</span>
+                      </h2>
+                      <p>
+                        I have completed my secondary education from ABS INTERNATIONAL SCHOOL orile iganmu lagos state with major subjects as
+                        Physics,Chemistry & Maths .{' '}
+                      </p>
+                    </div>
+                  </article>
+                  {/* </ScrollAnimation> */}
+
+                  {/* <ScrollAnimation
+                      offset={0}
+                      animateIn='fadeInLeft'
+                      duration={2.4}
+                      animateOnce={true}
+                      initiallyVisible={true}
+                    > */}
+                  <article>
+                    <div className={`${classes.timeline_icon} ${classes.timeline_icon_4}`}>
+                      <MdWork />
+                    </div>
+                    <div className={classes.label}>
+                      <h2>Internship</h2>
+                      <p>Completed three month Internship in web devlopment with Wifi-combat</p>
+                    </div>
+                    <div className={classes.timeline_entry_inner}>
+                      <div className={classes.timeline_icon_3 || classes.color_none}></div>
+                    </div>
+                  </article>
+                  {/* </ScrollAnimation> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* </ScrollAnimation> */}
+      </div>
+    );
+  }
+}
+
+export default Education;
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import 'animate.css/animate.min.css';
+import Tilt from 'react-parallax-tilt';
+// import ScrollAnimation from 'react-animate-on-scroll';
+import classes from './Projects.module.css';
+import github from '../images/github.png';
+import axnf from '../images/axnf.jpeg';
+import fl from '../images/cos.png';
+// Web projects
+const webItem = [
+  {
+    link: 'https://axionfoam.com/',
+    title: 'Axion',
+    techStack: 'Tech Stack: React js, bootstrap, javascript, node js ,microsoft sql',
+    desc: 'Axion is an e-commerce app for selling mattress online still under development..',
+    image: axnf,
+    color: '#D3D3D3',
+    githubLink: 'https://github.com/chuzzy888/AxionEcommerce.git'
+  },
+  {
+    link: 'https://confessionsite.netlify.app/',
+    title: 'Confess.io',
+    techStack: 'Tech Stack: React js, Tailwind css, javascript,firebase',
+    desc: 'a confession site where users can share secrets and make confessions ',
+    image: fl,
+    color: '#D3D3D3',
+    githubLink: 'https://github.com/chuzzy888/confession-site.git'
+  }
+];
+
+const androidAppItems = [];
+
+export default function Projects() {
+  const getProjectCard = (cardItem) => {
+    return (
+      <Tilt perspective={900} glareEnable={true} glareMaxOpacity={0.45}>
+        <li style={{ backgroundColor: cardItem.color, borderRadius: '21px' }}>
+          <a href={cardItem.link} className={classes.card} target='_blank'>
+            <img
+              src={cardItem.image}
+              className={classes.card__image}
+              alt=''
+              style={{ objectFit: 'contain' }}
+            />
+          </a>
+          <div
+            onClick={() => {
+              if (cardItem?.githubLink) window.open(cardItem?.githubLink, '_blank');
+            }}
+            className={classes.card__title__container}
+            style={{ padding: '10px' }}
+          >
+            <h3
+              className={classes.card__title}
+              style={{
+                color: 'white',
+                backgroundColor: 'black',
+                padding: '5px',
+                marginTop: '8px',
+                borderRadius: '5px'
+              }}
+            >
+              {cardItem.title}
+            </h3>
+
+            {cardItem?.githubLink && <img src={github} className={classes.card__title__img} />}
+          </div>
+          {/* <p
+            className={classes.card__desc}
+            style={{ fontSize: '13px ', textAlign: 'center', color: 'black', fontWeight: '900' }}
+          >
+            {cardItem.desc}
+          </p>
+          <p
+            className={classes.card__techStack}
+            style={{ fontSize: '10px ', textAlign: 'center', fontWeight: '900' }}
+          >
+            {cardItem.techStack}
+          </p> */}
+        </li>
+      </Tilt>
+    );
+  };
+
+  return (
+    <div className={classes.box} id='projects'>
+      {/* <ScrollAnimation
+        offset={0}
+        animateIn='fadeInLeft'
+        duration={2.4}
+        animateOnce={true}
+        initiallyVisible={true}
+      > */}
+      <span className={classes.head}>MY WORK</span>
+      <h2 className={classes.heading}>MY PROJECTS</h2>
+      <ul className={classes.cards}>
+        {webItem.map((item) => {
+          return getProjectCard(item);
+        })}
+      </ul>
+      <h2 className={classes.heading}></h2>
+      <ul className={classes.cards}>
+        {androidAppItems.map((item) => {
+          return getProjectCard(item);
+        })}
+      </ul>
+      {/* </ScrollAnimation> */}
+    </div>
+  );
+}
